3b9d2e16db9f1e4a9a4f016e9024330e883c02a6
Adam Gruda <adamgruda1312@gmail.com>
2023-06-26 23:42:32 +0200

    App with sample doctor and users

--- app/src/main/java/com/ewizyta/DoctorHomeFragment.kt
+++ app/src/main/java/com/ewizyta/DoctorHomeFragment.kt
@@ -1,16 +1,45 @@
 package com.ewizyta
 
+import UserAdapter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
 
 class DoctorHomeFragment : Fragment() {
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var userAdapter: UserAdapter
+    private lateinit var userList: List<User>
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_doctor, container, false)
+        val view = inflater.inflate(R.layout.fragment_home_doctor, container, false)
+
+        // Inicjalizacja listy lekarzy (przykładowe dane)
+        userList = createUserList()
+
+        // Inicjalizacja RecyclerView
+        recyclerView = view.findViewById(R.id.patientRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        // Inicjalizacja adaptera RecyclerView i przekazanie listy lekarzy
+        userAdapter = UserAdapter(userList)
+        recyclerView.adapter = userAdapter
+
+        return view
     }
 
+    private fun createUserList(): List<User> {
+        val userList = mutableListOf<User>()
+
+        // Dodawanie przykładowych lekarzy do listy
+        userList.add(User("John", "Doe", "a@wp.pl", "123", "123456789", "13.12.2001", "Mężczyzna"))
+        // Dodaj inne przykładowe lekarzy według potrzeb
+
+        return userList
+    }
 }
